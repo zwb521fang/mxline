187f370f0b758dee79e937a7d3bd0ed94933df2b
zwb521fang <591709393@qq.com>
2018-01-09 21:53:25 +0800

    python-edu

--- users/models.py
+++ users/models.py
@@ -9,7 +9,7 @@ class UserProfile(AbstractUser):
     nick_name = models.CharField(max_length=50,verbose_name=u"昵称",default='')
     birday  = models.DateField(verbose_name=u"生日",null=True,blank=True)
     gender = models.CharField(max_length =5,choices=(("male",u"男"),("female",u'女')),default='female')
-    address = models.CharField(max_length=100,default=u'')
+    address = models.CharField(max_length=100,default=u'地址')
     mobile = models.CharField(max_length=11,null=True,blank=True)
     image = models.ImageField(upload_to="image/%y/%m",default=u"image/default.png",max_length=100)
     class Meta:
